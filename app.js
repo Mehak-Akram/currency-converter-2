a58404ba58710744b2aa80bf3aed830f2447e773
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-09-24 23:23:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const currency = {
+    USD : 1, 
+    EUR : 0.91,
+    GBD : 0.76,
+    INR : 74.57,
+    PKR : 280,
+    UAE : 3.67,
+    JYP : 113,
+};
+
+document.getElementById("converter-form").addEventListener("submit",function(e){
+    e.preventDefault()
+    let fromCurrency = document.getElementById("from").value
+    let toCurrency = document.getElementById("to").value
+    let amount = document.getElementById("amount").value
+
+let fromAmount = currency[fromCurrency]
+let toAmount = currency[toCurrency]
+let baseAmount = amount/ fromAmount
+let convertAmount = baseAmount * toAmount
+
+document.getElementById("result").textContent = `Coverted Amount : ${Math.round(convertAmount)}`
+})
